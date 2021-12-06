6c430ae5c0adb20a783540d4a666155581667ee4
AlerjDiti <infraestrutura@alerj.rj.gov.br>
2021-12-06 17:28:11 -0300

    Minha primeira ação está pronta

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
+PASS=$(echo -n $2 | sha256sum | awk '{print $1}')
+HASH='bc4e6d8d87b1b153d104856da3e558909882eb984eef3e09596a6b839fd9114c'
 
-if [ $2 = $3 ]; then
+if [ $PASS = $HASH ]; then
     echo "Hello $1"
 	time=$(date)
 	echo "::set-output name=time::$time"
 else
     echo "Sem permissão para excutar o script."
 fi
-
-
