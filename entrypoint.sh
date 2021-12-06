3bd0dc1f3dbd64171b5dfa3a2adeb2d0917a61ae
AlerjDiti <infraestrutura@alerj.rj.gov.br>
2021-12-06 15:50:29 -0300

    Minha primeira ação está pronta

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+
+if [ $2 = $3 ]; then
+    echo "Hello $1"
+	time=$(date)
+	echo "::set-output name=time::$time"
+else
+    echo "Sem permissão para excutar o script."
+fi
+
+
